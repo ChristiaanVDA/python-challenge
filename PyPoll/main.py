2b53ad4bc1bf064cdf2bbd1ec147682734d2cc77
ChristiaanVDA <30744165+ChristiaanVDA@users.noreply.github.com>
2017-12-02 07:55:14 -0800

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,70 @@
+## PyPoll
+## Christiaan Van den Akker
+
+# import modules
+import os
+import csv
+import collections
+
+# Create lists to store column data
+Voter_ID = []
+County = []
+Candidate = []
+
+# Ask user which file to open and transform
+user_input = input("Which polling results would you like to open? 1 or 2 ? " )
+
+# Set the paths for the two csv files
+csvpath1 = os.path.join("raw_data","election_data_1.csv")
+csvpath2 = os.path.join("raw_data","election_data_2.csv")
+
+# Tie the user's choice to the file to be opened.
+if user_input=="1":
+    file_choice = csvpath1
+    print("You have chosen election_data_1")
+elif user_input=="2":
+    file_choice = csvpath2
+    print("You have chosen election_data_2")
+else:
+    file_choice = csvpath2
+    print("You have not made a valid choice. Opening election_data_2")
+   
+# Open the csv file
+with open(file_choice, newline ='') as csvfile1:
+    csvreader1 = csv.reader(csvfile1, delimiter=",")
+  
+    # Skip the header row
+    next(csvreader1)
+
+    # Create lists for each column
+    for row in csvreader1:
+        Voter_ID.append(row[0])
+        County.append(row[1])
+        Candidate.append(row[2])
+        
+    # Count the total voters
+    voter_count = len(Voter_ID) 
+    print("Voter Count: " + str(voter_count))
+    
+    # Get a list of the unique candidates
+    set_candidates = set(Candidate)
+    candidate_list = list(set_candidates)
+  
+    unique_candidates = set(set_candidates)
+    
+    
+    print (unique_candidates)
+
+    # Write to the text file
+with open("Output.txt", "w") as text_file:
+    text_file.write("Election Results" + "\n")
+    text_file.write("-------------------------" + "\n")
+    text_file.write("Total Votes: " + str(voter_count) + "\n")
+    text_file.write("-------------------------" + "\n")
+
+    
+
+
+
+
 
